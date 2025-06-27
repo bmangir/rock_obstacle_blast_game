dd3ab7d73431f1c903fb49cd7d8a8d1c2f61fb13
bmangir <berkantmangir23@gmail.com>
2025-06-27 14:56:35 +0300

    create main scene, add event to btn to direct level scene

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+namespace Managers
+{
+    public class LevelManager : MonoBehaviour
+    {
+        public static LevelManager Instance { get; private set; }
+
+        private const string LastPlayedLevelKey = "LastPlayedLevel";
+        public int CurrentLevel { get; private set; }
+        public int MaxLevel = 10;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            LoadCurrentLevel();
+        }
+
+        private void LoadCurrentLevel()
+        {
+            CurrentLevel = PlayerPrefs.GetInt(LastPlayedLevelKey, 1);
+        }
+
+        public void SetCurrentLevel(int level)
+        {
+            CurrentLevel = Mathf.Clamp(level, 1, MaxLevel);
+            PlayerPrefs.SetInt(LastPlayedLevelKey, CurrentLevel);
+            PlayerPrefs.Save();
+        }
+
+        public bool AllLevelsFinished()
+        {
+            return CurrentLevel > MaxLevel;
+        }
+
+#if UNITY_EDITOR
+        [UnityEditor.MenuItem("BlastGame/Set Last Played Level/Level 1")]
+        public static void SetLevel1() => SetLevelFromEditor(1);
+
+        [UnityEditor.MenuItem("BlastGame/Set Last Played Level/Level 10")]
+        public static void SetLevel10() => SetLevelFromEditor(10);
+
+        private static void SetLevelFromEditor(int level)
+        {
+            PlayerPrefs.SetInt(LastPlayedLevelKey, level);
+            PlayerPrefs.Save();
+            Debug.Log($"Set LastPlayedLevel to {level}");
+        }
+#endif
+    }
+}
---
+++ Assets/Scripts/Managers/MainMenuController.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Managers
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI levelButtonText;
+        [SerializeField] private Button levelButton;
+
+        private void Start()
+        {
+            if (LevelManager.Instance.AllLevelsFinished())
+            {
+                levelButtonText.text = "Finished";
+                levelButton.interactable = false;
+            }
+            else
+            {
+                levelButtonText.text = $"Level {LevelManager.Instance.CurrentLevel}";
+                levelButton.interactable = true;
+                levelButton.onClick.AddListener(OnLevelButtonClicked);
+            }
+        }
+
+        private void OnLevelButtonClicked()
+        {
+            SceneManager.LoadScene("LevelScene");
+        }
+    }
+}
