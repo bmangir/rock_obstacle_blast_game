e5095f1a6821938c2a3c3351323aaaf7dd2542b8
bmangir <berkantmangir23@gmail.com>
2025-06-27 21:26:35 +0300

    use background for level scene

---
+++ Assets/Scripts/Managers/BackgroundManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace Managers
+{
+    public class BackgroundManager : MonoBehaviour
+    {
+        [SerializeField] private string backgroundPath = "Menu/background";
+        [SerializeField] private int sortingOrder = -10;
+
+        void Start()
+        {
+            Sprite sprite = Resources.Load<Sprite>(backgroundPath);
+            if (sprite == null)
+            {
+                Debug.LogError($"Background not found at Resources/{backgroundPath}.png");
+                return;
+            }
+
+            SpriteRenderer sr = gameObject.AddComponent<SpriteRenderer>();
+            sr.sprite = sprite;
+            sr.sortingOrder = sortingOrder;
+
+            FitSpriteToScreen(sr);
+        }
+
+        private void FitSpriteToScreen(SpriteRenderer sr)
+        {
+            Camera cam = Camera.main;
+
+            float screenHeight = 3.25f * cam.orthographicSize;
+            float screenWidth = screenHeight * cam.aspect;
+
+            float spriteHeight = sr.sprite.bounds.size.y;
+            float spriteWidth = sr.sprite.bounds.size.x;
+
+            float scaleX = screenWidth / spriteWidth;
+            float scaleY = screenHeight / spriteHeight;
+
+            float scale = Mathf.Max(scaleX, scaleY); 
+
+            transform.localScale = new Vector3(scale, scale, 1f);
+            transform.position = new Vector3(0, 0, 10); // send to back
+        }
+    }
+}
